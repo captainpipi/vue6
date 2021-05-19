9b3ec8715a641117f2a4b86fff29dfc4b1a60943
captainpipi <1844547782@qq.com>
2021-05-19 21:16:25 +0800

    格式修改

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,34 @@
 <template>
-  <div id="app">
-    <!-- 路由 -->
-    <div>
-      <router-view/><!-- 登陆注册页面 , 已经登陆页面 -->
-    </div>
+    <div id="app">
+        <!-- 路由 -->
+        <div>
+            <router-view/><!-- 登陆注册页面 , 已经登陆页面 -->
+        </div>
 
-    <!-- 组件引用 -->
-    <div>
-      <end></end>
+        <!-- 组件引用 -->
+        <div>
+            <end></end>
+        </div>
     </div>
-  </div>
 </template>
 <script>
 
 import end from './comutils/end'
 
 export default {
-  name: 'App',
-  components: {
-    end
-  },
-  created(){
-     this.$router.push('/login_zhuce').catch(data => {  });//路由跳转
-  }
+    name: 'App',
+    components: {
+        end
+    },
+    created() {
+        this.$router.push('/login_zhuce').catch(data => {
+        });//路由跳转
+    }
 }
 </script>
 
 <style>
-  body{
+body {
     margin: 0;
-  }
+}
 </style>
